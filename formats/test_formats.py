ee86c9c99f343221b54af8d1ca79764f9058f95e
kremrik <kyle.robert.e@gmail.com>
2021-11-19 09:01:43 -0600

    moved all tests under one case

--- formats/test_formats.py
+++ formats/test_formats.py
@@ -4,7 +4,7 @@ from formats.formats import COLORS, STYLES
 import unittest
 
 
-class TestColors(unittest.TestCase):
+class TestColorsAndStyles(unittest.TestCase):
     def test_all_colors(self):
         for color, code in COLORS.items():
             with self.subTest(msg=color):
@@ -12,8 +12,6 @@ class TestColors(unittest.TestCase):
                 actual = getattr(Colors, color)("FOO")
                 self.assertEqual(expected, actual)
 
-
-class TestStyles(unittest.TestCase):
     def test_all_styles(self):
         for style, code in STYLES.items():
             with self.subTest(msg=style):
@@ -21,8 +19,6 @@ class TestStyles(unittest.TestCase):
                 actual = getattr(Styles, style)("FOO")
                 self.assertEqual(expected, actual)
 
-
-class TestComposition(unittest.TestCase):
     def test_color_and_style(self):
         expected = "\033[31;4mFOO\033[0m"
         actual = Colors.red(Styles.underline("FOO"))
