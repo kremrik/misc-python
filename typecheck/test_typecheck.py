0c4f853cae09917e43641d4751a5a58c3dc6088e
kremrik <kyle.robert.e@gmail.com>
2021-11-23 17:13:07 -0600

    module for parsing all given args to callable

---
+++ typecheck/test_typecheck.py
@@ -0,0 +1,149 @@
+from typecheck.typecheck import parse_inputs, Input, TypeCheckError
+
+import unittest
+
+
+class test_parse_inputs_happy_path(unittest.TestCase):
+    def test_no_params(self):
+        def fnc(): pass
+        expect = Input()
+        actual = parse_inputs(fnc)
+        self.assertEqual(expect, actual)
+
+    def test_one_arg(self):
+        def fnc(x): pass
+        expect = Input(
+            args={"x": 1}
+        )
+        actual = parse_inputs(fnc, args=[1])
+        self.assertEqual(expect, actual)
+
+    def test_mult_arg(self):
+        def fnc(x, y, z): pass
+        expect = Input(
+            args={"x": 1, "y": 2, "z": 3}
+        )
+        actual = parse_inputs(fnc, args=[1, 2, 3])
+        self.assertEqual(expect, actual)
+
+    def test_one_varargs(self):
+        def fnc(*args): pass
+        expect = Input(
+            varargs=[1]
+        )
+        actual = parse_inputs(fnc, args=[1])
+        self.assertEqual(expect, actual)
+
+    def test_mult_varargs(self):
+        def fnc(*args): pass
+        expect = Input(
+            varargs=[1, 2, 3]
+        )
+        actual = parse_inputs(fnc, args=[1, 2, 3])
+        self.assertEqual(expect, actual)
+
+    def test_args_and_varargs(self):
+        def fnc(x, y, *args): pass
+        expect = Input(
+            args={"x": 1, "y": 2},
+            varargs=[3, 4]
+        )
+        actual = parse_inputs(fnc, args=[1, 2, 3, 4])
+        self.assertEqual(expect, actual)
+
+    def test_one_kwonlyargs(self):
+        def fnc(*, x): pass
+        expect = Input(
+            kwonlyargs={"x": 1}
+        )
+        actual = parse_inputs(fnc, kwargs={"x": 1})
+        self.assertEqual(expect, actual)
+
+    def test_mult_kwonlyargs(self):
+        def fnc(*, x, y, z): pass
+        expect = Input(
+            kwonlyargs={"x": 1, "y": 2, "z": 3}
+        )
+        actual = parse_inputs(fnc, kwargs={"x": 1, "y": 2, "z": 3})
+        self.assertEqual(expect, actual)
+
+    def test_one_varkw(self):
+        def fnc(**kwargs): pass
+        expect = Input(
+            varkw={"x": 1}
+        )
+        actual = parse_inputs(fnc, kwargs={"x": 1})
+        self.assertEqual(expect, actual)
+
+    def test_mult_varkw(self):
+        def fnc(**kwargs): pass
+        expect = Input(
+            varkw={"x": 1, "y": 2, "z": 3}
+        )
+        actual = parse_inputs(fnc, kwargs={"x": 1, "y": 2, "z": 3})
+        self.assertEqual(expect, actual)
+
+    def test_kwonlyargs_and_varkw(self):
+        def fnc(*, i, j, **kwargs): pass
+        expect = Input(
+            kwonlyargs={"i": -1, "j": 0},
+            varkw={"x": 1, "y": 2}
+        )
+        actual = parse_inputs(
+            fnc, kwargs={"i": -1, "j": 0, "x": 1, "y": 2}
+        )
+        self.assertEqual(expect, actual)
+
+    def test_args_varargs_kwonlyargs_and_varkw(self):
+        def fnc(a, b, *args, c, d, **kwargs): pass
+        expect = Input(
+            args={"a": 1, "b": 2},
+            varargs=[3, 4],
+            kwonlyargs={"c": 3, "d": 4},
+            varkw={"e": 5, "f": 6}
+        )
+        actual = parse_inputs(
+            fnc, 
+            args=[1, 2, 3, 4],
+            kwargs={"c": 3, "d": 4, "e": 5, "f": 6}
+        )
+        print(actual)
+        self.assertEqual(expect, actual)
+
+
+class test_parse_inputs_exceptions(unittest.TestCase):
+    def test_no_params_and_given_args(self):
+        def fnc(): pass
+        with self.assertRaises(TypeCheckError):
+            parse_inputs(fnc, args=[1])
+
+    def test_no_params_and_given_args_and_kwargs(self):
+        def fnc(): pass
+        with self.assertRaises(TypeCheckError):
+            parse_inputs(fnc, args=[1], kwargs={"a": 1})
+
+    def test_extra_args(self):
+        def fnc(x): pass
+        with self.assertRaises(TypeCheckError):
+            parse_inputs(fnc, args=[1, 2])
+
+    def test_missing_args(self):
+        def fnc(x, y): pass
+        with self.assertRaises(TypeCheckError):
+            parse_inputs(fnc, args=[1])
+
+    def test_all_kwonlyargs_no_kwargs(self):
+        def fnc(*, x): pass
+        with self.assertRaises(TypeCheckError):
+            parse_inputs(fnc)
+
+    def test_missing_kwonlyargs(self):
+        def fnc(*, x): pass
+        with self.assertRaises(TypeCheckError):
+            parse_inputs(fnc)
+
+    def test_extra_kwonlyargs(self):
+        def fnc(*, x): pass
+        with self.assertRaises(TypeCheckError):
+            parse_inputs(fnc, kwargs={"x": 1, "y": 2})
+    
---
+++ typecheck/typecheck.py
@@ -0,0 +1,120 @@
+from dataclasses import dataclass, field
+from functools import wraps
+from inspect import getfullargspec, FullArgSpec
+from typing import Any, Callable, Dict, List, Optional
+
+
+@dataclass(frozen=True)
+class Input:
+    args: dict = field(default_factory=dict)
+    varargs: list = field(default_factory=list)
+    kwonlyargs: dict = field(default_factory=dict)
+    varkw: dict = field(default_factory=dict)
+
+
+class TypeCheckError(TypeError):
+    pass
+
+
+def parse_inputs(
+    fnc: Callable,
+    args: Optional[tuple] = None,
+    kwargs: Optional[dict] = None,
+) -> Input:
+    spec = get_fnc_spec(fnc)
+
+    args_and_varargs = process_args(spec, args)
+    kwonlyargs_and_varkw = process_kwargs(spec, kwargs)
+
+    all_inputs = {**args_and_varargs, **kwonlyargs_and_varkw}
+
+    return Input(**all_inputs)
+
+
+def process_args(
+    spec: FullArgSpec, args: tuple
+) -> dict:
+    s_args = spec.args
+    s_varargs = spec.varargs
+
+    output = {
+        "args": {},
+        "varargs": [],
+    }
+
+    err_msg = f"Callable takes positional argument(s) {s_args}, but was given {args}"
+
+    if not args and s_args:
+        raise TypeCheckError(err_msg)
+    elif not args and not s_args:
+        return output
+
+    if len(args) < len(s_args):
+        raise TypeCheckError(err_msg)
+
+    if len(args) == len(s_args):
+        output["args"] = dict(zip(s_args, args))
+        return output
+
+    if len(args) > len(s_args) and not s_varargs:
+        raise TypeCheckError(err_msg)
+
+    if len(args) > len(s_args) and s_varargs:
+        cutoff = len(s_args)
+        o_args = dict(zip(s_args, args[:cutoff]))
+        o_varargs = list(args[cutoff:])
+        output["args"] = o_args
+        output["varargs"] = o_varargs
+        return output
+
+
+def process_kwargs(
+    spec: FullArgSpec, kwargs: dict
+) -> dict:
+    s_kwonlyargs = spec.kwonlyargs
+    s_varkw = spec.varkw
+
+    output = {
+        "kwonlyargs": {},
+        "varkw": {},
+    }
+
+    err_msg = f"Callable takes keyword argument(s) {s_kwonlyargs}, but was given {kwargs}"
+
+    if not kwargs and s_kwonlyargs:
+        raise TypeCheckError(err_msg)
+    elif not kwargs and not s_kwonlyargs:
+        return output
+
+    missing = list(set(s_kwonlyargs) - set(kwargs))
+    extra = list(set(kwargs) - set(s_kwonlyargs))
+
+    if missing:
+        raise TypeCheckError(err_msg)
+
+    if not missing and not extra:
+        output["kwonlyargs"] = kwargs
+        return output
+
+    if extra and not s_varkw:
+        raise TypeCheckError(err_msg)
+
+    if extra and s_varkw:
+        o_kwonlyargs = {
+            k: v
+            for k, v in kwargs.items()
+            if k in s_kwonlyargs
+        }
+        o_varkw = {
+            k: v
+            for k, v in kwargs.items()
+            if k in extra
+        }
+        output["kwonlyargs"] = o_kwonlyargs
+        output["varkw"] = o_varkw
+        return output
+
+
+# ---------------------------------------------------------
+def get_fnc_spec(fnc: Callable) -> FullArgSpec:
+    return getfullargspec(fnc)
