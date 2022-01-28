cf3b44c856b09c1b474b66dd14d2ba96d9e9d9c2
kremrik <kyle.robert.e@gmail.com>
2022-01-28 16:00:26 -0600

    making sure bytes also work with handler for chunkstream

--- chunkstream/test_chunkstream.py
+++ chunkstream/test_chunkstream.py
@@ -1,15 +1,21 @@
 from chunkstream import ChunkGen
 
 import unittest
-from io import StringIO
+from io import BytesIO, StringIO
 from textwrap import dedent
 
 
 def buffered_char_stream(handler, size=16):
     segment = handler.read(size)
+
+    if isinstance(segment, bytes):
+        xformer = lambda x: chr(x)
+    else:
+        xformer = lambda x: x
+
     while segment:
         for char in segment:
-            yield char
+            yield xformer(char)
         segment = handler.read(size)
 
 
@@ -61,3 +67,34 @@ class TestChunkGen(unittest.TestCase):
         with self.assertRaises(IOError):
             for char in buffer:
                 cg(char)
+
+    def test_with_bytes(self):
+        bytez = (
+            dedent(
+                """\
+        <root>
+            <crap>
+                <tag>hello world</tag>
+            </crap>
+            <crap>
+                <tag>goodbye world</tag>
+            </crap>
+        </root>
+        """
+            )
+            .strip()
+            .encode()
+        )
+        handler = BytesIO(bytez)
+        buffer = buffered_char_stream(handler)
+        cg = ChunkGen("<tag>", "</tag>")
+
+        expect = [
+            "<tag>hello world</tag>",
+            "<tag>goodbye world</tag>",
+        ]
+        actual = []
+        for char in buffer:
+            if chunk := cg(char):
+                actual.append(chunk)
+        self.assertEqual(expect, actual)
